87fcd4e26514313506bbb536c434f775ca46679d
Nate Anderson <curtislowe10@gmail.com>
2016-11-02 17:51:13 -0600

    add signup and login routes with test

--- spec/features/user_can_see_all_weather_info_for_destination_spec.rb
+++ spec/features/user_can_see_all_weather_info_for_destination_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
-RSpec.feature "User can see all weather info for destination" do
-  scenario "on the specific destination page" do
+RSpec.feature "visitor can sign up for the site" do
+  scenario "it can become a registered user" do
     visit '/'
 
-    click_on "Chicago"
+    click_on "Sign Up"
 
-    expect(current_path).to eq("/destinations/:id")
+    expect(current_path).to eq("/signup")
 
-    expect(page).to have_content("Chicago")
-    expect(page).to have_content("60120")
-    expect(page).to have_content("Chicago")
-    expect(page).to have_content("Chicago")
+    expect(page).to have_css('form')
+
+    fill_in "First name", with: "Nate"
+    fill_in "Last name", with: "Anderson"
+    fill_in "Email", with: "blah@gmail.com"
+    fill_in "Password", with: "password"
+
+    click_button "Create Your Account"
+
+    expect(current_path).to eq('/')
   end
 end
