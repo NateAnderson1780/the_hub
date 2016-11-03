e7be6d3d5b6b1e7e31a44ef8cba103f560cbfad0
Nate Anderson <curtislowe10@gmail.com>
2016-11-03 09:20:56 -0600

    add visitor can sign up for site test and functionality

--- spec/features/visitor_can_sign_up_for_site.rb
+++ spec/features/visitor_can_sign_up_for_site.rb
@@ -15,7 +15,7 @@ RSpec.feature "visitor can sign up for the site" do
     fill_in "Email", with: "blah@gmail.com"
     fill_in "Password", with: "password"
 
-    click_button "Create Your Account"
+    click_button "Create My Account"
 
     expect(current_path).to eq('/')
   end
