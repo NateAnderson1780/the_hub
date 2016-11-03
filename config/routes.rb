ed5c07a76cac0cc46b2e0ca9a59f593601873acc
Nate Anderson <curtislowe10@gmail.com>
2016-11-03 09:47:43 -0600

    change path for current user

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get 'signup', to: "users#new"
+
   get 'login', to: "sessions#new"
   post 'login', to: "sessions#create"
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create]
+
+  get 'teams', to: "teams#index"
+  get 'players', to: "players#index"
 end
