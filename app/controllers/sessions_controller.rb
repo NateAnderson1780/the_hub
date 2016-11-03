f99223dbf6f974605781e66ee8617b3b54c90d9f
Nate Anderson <curtislowe10@gmail.com>
2016-11-03 14:25:10 -0600

    initial twitter oauth setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email_address: params[:session][:email_address])
-    if user && user.authenticate(params[:session][:password])
+    if request.env["omniauth.auth"]
+      user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
       redirect_to dashboard_path
     else
-      flash.now[:danger] = "Email and/or Password is invalid, please try again."
-      render :new
+      user = User.find_by(email_address: params[:session][:email_address])
+      if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        redirect_to dashboard_path
+      else
+        flash.now[:danger] = "Email and/or Password is invalid, please try again."
+        render :new
+      end
     end
   end
 
