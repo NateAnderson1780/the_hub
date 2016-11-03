382a6cc812c22670bfb6b961eeedca6e6f0e5a0c
Nate Anderson <curtislowe10@gmail.com>
2016-11-03 11:05:53 -0600

    add visitor homepage spec

---
+++ spec/features/visitor_can_visit_homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "visitor can visit the homepage" do
+  scenario "it can see links to login and sign up" do
+    visit '/'
+
+    within(".navbar") do
+      expect(page).to have_link("Login")
+      expect(page).to have_link("Sign Up")
+    end
+  end
+end
