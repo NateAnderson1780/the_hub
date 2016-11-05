117491b39b4917891813feceed2ef5361a1fbf42
Nate Anderson <curtislowe10@gmail.com>
2016-11-05 15:28:16 -0600

    add current game functionality on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,56 @@
 class HomeController < ApplicationController
   def index
-    
+    # response = Faraday.get("http://api.sportradar.us/nba-t3/games/2016/11/04/schedule.json?api_key=#{ENV['sport_radar_api_key']}")
+    # parsed_response = JSON.parse(response.body, symbolize_names: true)
+    # @games = parsed_response[:games].map do |game|
+    #   Game.new(game)
+    # end
+    # response = Faraday.whatever...
+    # parsed_response = Json.parsed_res...
+    # binding.pry
+    # # Game.new_get
+    # @games = parsed_response.map do |game|
+    #   Game.new
+    # end
+
+    # @games = Game.get_games(date or Time.now)
+    # @teams = Team.get_teams
+
+    # @presenter = Presenter.new(date)
   end
 end
+
+# view...
+#
+# @presenter.games
+#
+# @presenter.teams
+#
+# class Presenter
+#
+#   def initialize(date)
+#     @date = date
+#   end
+#
+#   def game
+#     Game.get_games(date)
+#   end
+#
+#   def teams
+#     Team.get_teams
+#   end
+#
+# end
+#
+# class Game
+#   def self.get_games(date)
+#     game_hash = SportService.get_games(date)
+#     game_hash.map do |game|
+#       Game.new(game)
+#   end
+# end
+#
+# class SportService
+#
+#   def self.get_games(date)
+#     #Faraday.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+class Seed
+  def self.start
+    Team.delete_all
+    generate_teams
+  end
+
+  def self.generate_teams
+    teams = ["Atlanta Hawks",
+             "Boston Celtics",
+             "Charlotte Hornets",
+             "Chicago Bulls",
+             "Cleveland Cavaliers",
+             "Dallas Mavericks",
+             "Denver Nuggets",
+             "Detroit Pistons",
+             "Golden State Warriors",
+             "Houston Rockets",
+             "Indiana Pacers",
+             "LA Clippers",
+             "LA Lakers",
+             "Memphis Grizzlies",
+             "Miami Heat",
+             "Milwaukee Bucks",
+             "Minnesota Timberwolves",
+             "New Jersey Nets",
+             "New Orleans Pelicans",
+             "New York Knicks",
+             "Oklahoma City Thunder",
+             "Orlando Magic",
+             "Philadelphia 76ers",
+             "Phoenix Suns",
+             "Portland Trailblazers",
+             "Sacramento Kings",
+             "San Antonio Spurs",
+             "Toronto Raptors",
+             "Utah Jazz",
+             "Washington Wizards"]
+    teams.each do |team|
+      Team.create(name: team)
+    end
+    puts "Created Teams"
+    assign_codes_to_cities
+  end
+
+  def self.assign_codes_to_cities
+    City.find_by(name: "Colorado Springs").update_attribute(:image_url, 'http://i.imgur.com/YGkazHV.jpg')
+  end
+end
