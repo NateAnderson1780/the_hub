0edd2b4e1a411de8a06445ccb62bf405761a70cf
Nate Anderson <curtislowe10@gmail.com>
2016-11-06 13:01:27 -0700

    add migration for userteams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105213331) do
+ActiveRecord::Schema.define(version: 20161105234426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,4 +36,15 @@ ActiveRecord::Schema.define(version: 20161105213331) do
     t.string   "oauth_token_secret"
   end
 
+  create_table "userteams", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_userteams_on_team_id", using: :btree
+    t.index ["user_id"], name: "index_userteams_on_user_id", using: :btree
+  end
+
+  add_foreign_key "userteams", "teams"
+  add_foreign_key "userteams", "users"
 end
