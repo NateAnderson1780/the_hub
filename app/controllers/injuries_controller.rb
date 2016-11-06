2fe772cf2f4cc6d50bb26399e7425e8e1af06de7
Nate Anderson <curtislowe10@gmail.com>
2016-11-06 16:03:08 -0700

    add standings to traverse conference and divisions

--- app/controllers/injuries_controller.rb
+++ app/controllers/injuries_controller.rb
@@ -3,10 +3,10 @@ class InjuriesController < ApplicationController
     @team = Team.find(params[:team_id])
     response = Faraday.get("http://api.sportradar.us/nba-t3/league/injuries.json?api_key=#{ENV['sport_radar_api_key']}")
     data = JSON.parse(response.body, symbolize_names: true)
-    @injury_team = data[:teams].find do |raw_team|
+    injury_team = data[:teams].find do |raw_team|
       raw_team[:id] == @team.team_code
     end
-    @injuries = @injury_team[:players].map do |raw_player|
+    @injuries = injury_team[:players].map do |raw_player|
       Injury.new(raw_player)
     end
   end
