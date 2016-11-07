f6ece837296aaa9678cd0df0790a891ee58556f5
Nate Anderson <curtislowe10@gmail.com>
2016-11-06 19:21:44 -0700

    rename player model to roster_player

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -4,7 +4,7 @@ class RostersController < ApplicationController
     response = Faraday.get("http://api.sportradar.us/nba-t3/teams/#{@team.team_code}/profile.json?api_key=#{ENV['sport_radar_api_key']}")
     data = JSON.parse(response.body, symbolize_names: true)
     @players = data[:players].map do |raw_player|
-      Player.new(raw_player)
+      RosterPlayer.new(raw_player)
     end
   end
 end
