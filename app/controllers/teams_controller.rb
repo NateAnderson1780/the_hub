ba40a818576cfd445f1e7e22be24b2ba299c204e
Nate Anderson <curtislowe10@gmail.com>
2016-11-06 19:22:33 -0700

    alphabetize teams in view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,6 @@
 class TeamsController < ApplicationController
   def index
     @teams = Team.order("name")
-    @userteams = current_user.teams
+    @userteams = current_user.teams.order("name")
   end
-
 end
