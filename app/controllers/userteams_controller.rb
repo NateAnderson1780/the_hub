7700309041e776fbe90b4ed017726bd212f0695a
Nate Anderson <curtislowe10@gmail.com>
2016-11-06 19:23:01 -0700

    add destroy team functionality

--- app/controllers/userteams_controller.rb
+++ app/controllers/userteams_controller.rb
@@ -4,4 +4,9 @@ class UserteamsController < ApplicationController
     current_user.teams << team
     redirect_to teams_path
   end
+
+  def destroy
+    current_user.teams.delete(params[:id])
+    redirect_to teams_path
+  end
 end
