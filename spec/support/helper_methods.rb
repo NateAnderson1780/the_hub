c5e2f17de80e98ad2b91d29698efdfc7a4ff65f5
Nate Anderson <curtislowe10@gmail.com>
2016-11-07 15:02:37 -0700

    add helper method login_as_user_with_teams

--- spec/support/helper_methods.rb
+++ spec/support/helper_methods.rb
@@ -5,3 +5,20 @@ def login(user)
 
   click_button "Login"
 end
+
+def login_as_user_with_teams
+  user = create(:user)
+
+  visit '/login'
+  fill_in "Username", with: user.username
+  fill_in "Password", with: user.password
+
+  click_button "Login"
+  click_link "Teams"
+
+  select("Chicago Bulls", :class => "Chicago Bulls")
+  click_button "Add Team"
+
+  expect(current_path).to eq('/teams')
+  expect(page).to have_content("Team: Chicago Bulls")
+end
