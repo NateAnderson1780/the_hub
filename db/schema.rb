5e9edbb403cd1f96c770e4e6e2695ec8a730952f
Nate Anderson <curtislowe10@gmail.com>
2016-11-07 15:01:27 -0700

    add migration for userplayers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107003418) do
+ActiveRecord::Schema.define(version: 20161107194339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20161107003418) do
     t.string   "conference_code"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "userplayers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "player_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["player_id"], name: "index_userplayers_on_player_id", using: :btree
+    t.index ["user_id"], name: "index_userplayers_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,6 +61,8 @@ ActiveRecord::Schema.define(version: 20161107003418) do
     t.index ["user_id"], name: "index_userteams_on_user_id", using: :btree
   end
 
+  add_foreign_key "userplayers", "players"
+  add_foreign_key "userplayers", "users"
   add_foreign_key "userteams", "teams"
   add_foreign_key "userteams", "users"
 end
