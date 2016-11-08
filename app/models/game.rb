d1c4e374adfdf539db72998f91a5b083c45b246b
Nate Anderson <curtislowe10@gmail.com>
2016-11-08 13:10:44 -0700

    add by_schedule method to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,12 @@ class Game
     @game_data = raw_game
   end
 
+  def self.by_schedule(team_code)
+    SportRadarService.new.team_schedule(team_code).map do |game|
+        Game.new(game) if (game[:home][:id] == team_code || game[:away][:id] == team_code)
+    end.compact
+  end
+
   def venue
     "#{@game_data[:venue][:name]}, #{@game_data[:venue][:city]}, #{@game_data[:venue][:state]}"
   end
